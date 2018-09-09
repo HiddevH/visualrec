f77b72c9d0d671ac15db8b7ff4a69d175171a3dd
MR1991 <mauricerichard91@gmail.com>
2018-09-09 14:19:08 +0000

    latest change

--- myproject.py
+++ myproject.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/hi/')
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+@app.route('/bye/')
+def bye():
+    return "<h1 style='color:blue'>Bye!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
